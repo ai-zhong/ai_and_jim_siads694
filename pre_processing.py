646fdbe68e33f91d5f82bd864f8c05a00b9260e9
Ai Zhong <aizhong.ds@gmail.com>
2021-05-25 23:17:57 -0700

    update to add in more features, now 11 features

--- pre_processing.py
+++ pre_processing.py
@@ -1,13 +1,55 @@
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
-from train_wordvecs import tokenize
 import pandas as pd
 import numpy as np
 import pickle
-from train_wordvecs import load_data
 from tqdm import tqdm
+from nltk import pos_tag
 
-stops = stopwords.words('english')
+
+def load_data(filename='WikiLarge_Train.csv', read_partial=False, s=100000):
+    """
+    if read_partial, then only part of the data is read
+    s is the length of sample you want to take if read_partial==True
+    """
+
+    if read_partial:
+        # n is the total length of the file
+        n = sum(1 for line in open(filename)) - 1
+        skip = sorted(random.sample(range(1, n + 1), n - s))
+        df = pd.read_csv(filename, skiprows=skip)
+        return df
+
+    df = pd.read_csv(filename)
+
+    return df
+
+
+def tokenize(df, lemmatize=False):
+    """
+    some preprocessing before training word2vec
+    note we do NOT lower case word here
+    """
+    stops = stopwords.words('english')
+
+    re_tokenized = []
+    if not lemmatize:
+        for text in tqdm(df.original_text):
+            re_tokenized.append([x for x in re.findall(r'(\w+)', text)
+                                 if x.lower() not in stops and len(x) > 1])
+        pickle.dump(re_tokenized, open('re_tokenized_nolemma.pkl', 'wb'))
+
+    if lemmatize:
+        import spacy
+        nlp = spacy.load('en', disable=['parser', 'ner'])
+        for text in tqdm(df.original_text):
+            text = nlp(text)
+            re_tokenized.append([token.lemma_ for token in text
+                                 if str(token).lower() not in stops and len(str(token)) > 1
+                                 and not str(token.lemma_).startswith('-')])
+        pickle.dump(re_tokenized, open('re_tokenized_lemma.pkl', 'wb'))
+
+    return re_tokenized
 
 
 def load_external_resources():
@@ -25,9 +67,9 @@ def load_external_resources():
     dale = [x.strip() for x in dale]
 
     # file 2
-    aoa = pd.read_csv('AoA_51715_words.csv', encoding='iso-8859-1')
-    aoa = aoa[['Word', 'AoA_Kup_lem']]
-    aoa_dict = aoa.set_index('Word').to_dict()['AoA_Kup_lem']
+    aoa_df = pd.read_csv('AoA_51715_words.csv', encoding='iso-8859-1')
+    # aoa = aoa[['Word', 'AoA_Kup_lem']]
+    # aoa_dict = aoa.set_index('Word').to_dict()['AoA_Kup_lem']
 
     # file 3
     with open('Concreteness_ratings_Brysbaert_et_al_BRM.txt', 'r') as f:
@@ -42,7 +84,7 @@ def load_external_resources():
     float_cols = ['Bigram', 'Conc.M', 'Conc.SD', 'Unknown', 'Total', 'Percent_known', 'SUBTLEX']
     concrete_df[float_cols] = concrete_df[float_cols].astype(float)
 
-    return dale, aoa_dict, concrete_df
+    return dale, aoa_df, concrete_df
 
 
 def syllable_count(word):
@@ -96,33 +138,14 @@ def calc_dale_score(doc_tokens, doc_sent_cnt, dale_list):
     return (score, asw)
 
 
-def get_aoa_score(list_of_tokens, aoa_dict):
+def get_score_from_dict(list_of_tokens, score_dict):
     """
-    AoA: early (simpler) words have lower score
-    calculate the average score for a list of cleaned tokens
-    """
-    scores = []
-    for token in list_of_tokens:
-        if token.lower() in aoa_dict:
-            scores.append(aoa_dict[token.lower()])
-
-    if scores:
-        return np.nanmean(scores)
-
-    else:
-        return np.nan
-
-
-def get_concrete_score(list_of_tokens, concrete_dict):
-    """
-    One columns of interest: % of Raters Who Knew Word = concrete_known_score
-    Calculate the average score for a list of cleaned tokens
-    Higher average score means more individuals knew the word.
+    Calculate the average score for a list of cleaned tokens based on different score dictionaries
     """
     scores = []
     for token in list_of_tokens:
-        if token.lower() in concrete_dict:
-            scores.append(concrete_dict[token.lower()])
+        if token.lower() in score_dict:
+            scores.append(score_dict[token.lower()])
     if scores:
         return np.nanmean(scores)
     else:
@@ -140,15 +163,18 @@ def get_avg_word_length(list_of_tokens):
         return np.nan
 
 
-def calculate_scores(df, lemma=False, token_file='re_tokenized_'):
+def calculate_scores(df, lemma=False, token_file='re_tokenized_', train_or_test='Train'):
     """
-    generate scores for dale_chall, syllables count per word, and aoa.
+    generate numerical scores.
     df has a column named 're_tokened'
-
-    return df with 3 new columns of scores
     """
-    dale, aoa_dict, concrete_df = load_external_resources()
-    concrete_dict = concrete_df.set_index('Word').to_dict()['Percent_known']
+    dale, aoa_df, concrete_df = load_external_resources()
+
+    aoa_dict1 = aoa_df.set_index('Word').to_dict()['AoA_Kup_lem']
+    aoa_dict2 = aoa_df.set_index('Word').to_dict()['Freq_pm']
+    concrete_dict1 = concrete_df.set_index('Word').to_dict()['Percent_known']
+    concrete_dict2 = concrete_df.set_index('Word').to_dict()['Conc.M']
+    concrete_dict3 = concrete_df.set_index('Word').to_dict()['SUBTLEX']
 
     df['sentence_cnt'] = df.original_text.apply(lambda x: len(sent_tokenize(x)))
 
@@ -166,29 +192,65 @@ def calculate_scores(df, lemma=False, token_file='re_tokenized_'):
             if lemma:
                 df['re_tokened'] = tokenize(df, lemmatize=True, save_as=f'{token_file}lemma.pkl')
 
-        scores1 = []
-        scores2 = []
-        scores3 = []
-        scores4 = []
+        # 9 scores
+        dale_chall_scores = []
+        asw_scores = []
+        Percent_known = []
+        avg_word_lens = []
         aoa_scores = []
+        aoa_freqpms = []
+        conc_mean_scores = []
+        subtlex_scores = []
+        cnt_verbs_all = []
+
         for i in tqdm(range(len(df))):
             item = df.iloc[i]
+
             score1, score2 = calc_dale_score(item['re_tokened'], item['sentence_cnt'], dale)
-            scores3.append(get_concrete_score(item['re_tokened'], concrete_dict))
-            scores4.append(get_avg_word_length(item['re_tokened']))
-            scores1.append(score1)
-            scores2.append(score2)
+            dale_chall_scores.append(score1)
+            asw_scores.append(score2)
 
-            score = get_aoa_score(item['re_tokened'], aoa_dict)
-            aoa_scores.append(score)
+            Percent_known.append(get_score_from_dict(item['re_tokened'], concrete_dict1))
+            conc_mean_scores.append(get_score_from_dict(item['re_tokened'], concrete_dict2))
+            subtlex_scores.append(get_score_from_dict(item['re_tokened'], concrete_dict3))
 
-        df['dale_chall_score'] = scores1
-        df['syllable_per_word'] = scores2
-        df['concrete_score'] = scores3
-        df['average_word_len'] = scores4
+            avg_word_lens.append(get_avg_word_length(item['re_tokened']))
+            aoa_scores.append(get_score_from_dict(item['re_tokened'], aoa_dict1))
+            aoa_freqpms.append(get_score_from_dict(item['re_tokened'], aoa_dict2))
+
+            # count how many verbs appeared in this doc
+            tags = pos_tag(word_tokenize(df['original_text'].iloc[i]))
+            cnt_verbs = sum([x[-1][0]=='V' for x in tags])
+            cnt_verbs_all.append(cnt_verbs)
+
+
+        df['dale_chall_score'] = dale_chall_scores
+        df['syllable_per_word'] = asw_scores
+        df['concrete_score'] = Percent_known
+        df['conc_mean'] = conc_mean_scores
+        df['subtlex'] = subtlex_scores
+        df['average_word_len'] = avg_word_lens
         df['aoa'] = aoa_scores
+        df['aoa_freqpm'] = aoa_freqpms
+        df['verb2'] = cnt_verbs_all
 
-        return df
+        df['word_cnt'] = df['re_tokened'].apply(len)
+
+        # ============================================
+        # save results as csvs for later reloading
+        df[['dale_chall_score']].to_csv(f'WikiLarge_{train_or_test}_dale_chall_score2.csv')
+        df[['syllable_per_word']].to_csv(f'WikiLarge_{train_or_test}_syllable_per_w2.csv')
+        df[['concrete_score']].to_csv(f'WikiLarge_{train_or_test}_concrete_score2.csv')
+        df[['conc_mean']].to_csv(f'WikiLarge_{train_or_test}_conc_mean_score2.csv')
+        df[['subtlex']].to_csv(f'WikiLarge_{train_or_test}_conc_subtlex_score2.csv')
+
+        df[['average_word_len']].to_csv(f'WikiLarge_{train_or_test}_avg_word_len2.csv')
+        df[['aoa']].to_csv(f'WikiLarge_{train_or_test}_aoa2.csv')
+        df[['aoa_freqpm']].to_csv(f'WikiLarge_{train_or_test}_aoa_freqpm_scores2.csv')
+        df[['verb2']].to_csv(f'WikiLarge_{train_or_test}_verb_cnts2.csv')
+
+        df[['word_cnt']].to_csv(f'WikiLarge_{train_or_test}_word_count2.csv')
+
 
 
 def load_df_and_features(path, feature_names=None, train=True, lemma=True):
@@ -205,13 +267,15 @@ def load_df_and_features(path, feature_names=None, train=True, lemma=True):
     if feature_names is None:
         feature_names = ['aoa', 'avg_word_len', 'concreteness',
                          'dale_chall', 'syllable_per_w', 'doc_length', 
-                         'conc_mean', 'conc_subtlex', 'aoa_freqpm', 'word_cnt']
+                         'conc_mean', 'conc_subtlex', 'aoa_freqpm', 'word_cnt',
+                         'verb_cnt']
 
     features = []
     if train:
         df = load_data(filename='WikiLarge_Train.csv', read_partial=False)
 
         for f in feature_names:
+            # with lemmatization, document result is saved with 2 suffix
             if lemma:
                 if f == 'aoa':
                     feat = pd.read_csv(os.path.join(path, 'WikiLarge_Train_aoa2.csv'), index_col=0)
@@ -223,6 +287,8 @@ def load_df_and_features(path, feature_names=None, train=True, lemma=True):
                     feat = pd.read_csv(os.path.join(path, 'WikiLarge_Train_dale_chall_score2.csv'), index_col=0)
                 elif f=='syllable_per_w':
                     feat = pd.read_csv(os.path.join(path, 'WikiLarge_Train_syllable_per_w2.csv'), index_col=0)
+                
+                # this is not loaded but calculated.
                 elif f=='doc_length':
                     df['len'] = df['original_text'].apply(len)
                     continue
@@ -235,10 +301,10 @@ def load_df_and_features(path, feature_names=None, train=True, lemma=True):
                     feat = pd.read_csv(os.path.join(path, 'WikiLarge_Train_aoa_freqpm_scores2.csv'), index_col=0)
                 elif f=='word_cnt':
                     feat = pd.read_csv(os.path.join(path, 'WikiLarge_Train_word_count2.csv'), index_col=0)
-
+                elif f=='verb_cnt':
+                    feat = pd.read_csv(os.path.join(path, 'WikiLarge_Train_verb_cnts2.csv'), index_col=0)
                 else:
                     print(f'{f} is not one of calculated features')
-                    return None
                 features.append(feat)
 
         features = pd.concat(features, axis=1)
@@ -249,36 +315,6 @@ def load_df_and_features(path, feature_names=None, train=True, lemma=True):
 
 if __name__ == '__main__':
 
-    # df = load_data(filename='WikiLarge_Test.csv', read_partial=False, s=100000)
-    # df = calculate_scores(df)
-    # df[['dale_chall_score']].to_csv('WikiLarge_Train_dale_chall_score.csv')
-    # df[['syllable_per_word']].to_csv('WikiLarge_Train_syllable_per_w.csv')
-    # df[['aoa']].to_csv('WikiLarge_Train_aoa.csv')
-
-    # df = calculate_scores(df, lemma=True, token_file='re_tokenized_test_')
-    # df[['dale_chall_score']].to_csv('WikiLarge_Test_dale_chall_score2.csv')
-    # df[['syllable_per_word']].to_csv('WikiLarge_Test_syllable_per_w2.csv')
-    # df[['aoa']].to_csv('WikiLarge_Test_aoa2.csv')
-    # df[['concrete_score']].to_csv('WikiLarge_Train_concrete_score2.csv')
-    # df[['average_word_len']].to_csv('WikiLarge_Train_avg_word_len2.csv')
-
-
-    # # calculate new score features from Jim
-    # re_tokened = pickle.load(open('re_tokenized_lemma.pkl', 'rb'))
-    # dale, aoa_dict, concrete_df = load_external_resources()
-    # concrete_dict = concrete_df.set_index('Word').to_dict()['Percent_known']
-    #
-    # concrete_scores=[]
-    # avg_word_scores=[]
-    # for ls in tqdm(re_tokened):
-    #     concrete_scores.append(get_concrete_score(ls, concrete_dict))
-    #     avg_word_scores.append(get_avg_word_length(ls))
-    #
-    # sc1 = pd.DataFrame(concrete_scores, columns=['concrete_score'])
-    # sc2 = pd.DataFrame(avg_word_scores, columns=['average_word_len'])
-    #
-    # sc1.to_csv('WikiLarge_Train_concrete_score2.csv')
-    # sc2.to_csv('WikiLarge_Train_avg_word_len2.csv')
     test = load_df_and_features(path='')
     print(test.head())
     print(test.columns)
